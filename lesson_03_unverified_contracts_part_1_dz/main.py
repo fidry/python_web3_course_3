5731748e8cc861abf774bb06e7c063f29188cdb8
maked0n1an <promka4146@gmail.com>
2024-11-08 21:16:41 +0200

    Updated.

--- lesson_03_unverified_contracts_part_1_dz/main.py
+++ lesson_03_unverified_contracts_part_1_dz/main.py
@@ -14,22 +14,31 @@ async def main():
     )
 
     spacefi = SpaceFi(client)
-    # await spacefi.swap_eth_to_usdt(
-    #     token_amount=TokenAmount(0.0006),
-    #     slippage=5
-    # )
-    # await spacefi.swap_usdt_to_usdc_e(
-    #     slippage=4, 
-    #     is_all_balance=True
-    # )
+    await spacefi.swap_eth_to_usdt(
+        token_amount=TokenAmount(0.0006),
+        slippage=5
+    )
+    await spacefi.swap_eth_to_wbtc(
+        token_amount=TokenAmount(0.001),
+        slippage=5
+    )
+    await spacefi.swap_usdt_to_usdc_e(
+        slippage=5, 
+        is_all_balance=True
+    )
+    await spacefi.swap_usdc_e_to_eth(
+        token_amount=TokenAmount(2, decimals=6),
+        slippage=5,
+        is_all_balance=True
+    )
 
     spacefi = SpaceFiHard(client)
-    # await spacefi.swap(
-    #     from_token_symbol='WBTC',
-    #     to_token_symbol='ETH',
-    #     amount=0.000023,
-    #     slippage=5
-    # )
+    await spacefi.swap(
+        from_token_symbol='WBTC',
+        to_token_symbol='ETH',
+        amount=0.00004,
+        slippage=5
+    )
 
 if __name__ == "__main__":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
