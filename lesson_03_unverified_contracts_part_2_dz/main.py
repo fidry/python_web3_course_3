bb3d0919e7b6971cdd063133f2e1f0625c18acd2
maked0n1an <promka4146@gmail.com>
2024-11-14 20:11:13 +0200

    main: updated.

--- lesson_03_unverified_contracts_part_2_dz/main.py
+++ lesson_03_unverified_contracts_part_2_dz/main.py
@@ -3,6 +3,8 @@ from client import Client
 import config
 from data.models import TokenAmount
 from tasks.koi_finance import KoiFinance
+from tasks.maverick import Maverick
+from tasks.sync_swap import SyncSwap
 
 
 async def main():
@@ -13,8 +15,18 @@ async def main():
     )
     koi_finance = KoiFinance(client)
     await koi_finance.swap_eth_to_usdc(
-        token_amount=TokenAmount(0.002),
+        token_amount=TokenAmount(0.001),
         slippage=1
+    )
+    maverick = Maverick(client)
+    await maverick.swap_usdc_to_mav(
+        slippage=1,
+        is_all_balance=True
+    )
+    sync_swap = SyncSwap(client)
+    await sync_swap.swap_usdc_e_to_eth(
+        slippage=1,
+        is_all_balance=True
     )
 
 
