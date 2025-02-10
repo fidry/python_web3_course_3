3bd80d7b82a000c97d08b0b3fc30ce8413248ffb
crashnosok <riocrash@mail.ru>
2025-02-10 16:47:59 +0300

    remove redandunt comment

--- lesson_11_architecture/tasks/koi_finance.py
+++ lesson_11_architecture/tasks/koi_finance.py
@@ -110,8 +110,6 @@ class KoiFinance(Base):
             return f'{failed_text} | Can not get tx_hash_bytes'
 
         try:
-            # Transaction success (0.001 ETH -> 2.28988 USDC)!!
-            # tx_hash: 0x5e97aaaa972dc2aca2bdb8b6241fe6dd5bb9eaeb238d0dcd941c31c46198b51e
             tx_hash = await self.client.verif_tx(tx_hash=tx_hash_bytes)
             return (f'({router.title}) Transaction success! ({token_amount.Ether} ETH -> '
                     f'{amount_out_min.Ether} {to_token.title}) | tx_hash: {tx_hash}')
